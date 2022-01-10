91250e721fbc0792afeddf340a7fc1c86ef5aa1a
NgaoDaCoPho <87919718+NgaoDaCoPho@users.noreply.github.com>
2022-01-10 14:43:51 +0700

    Update getlink.js

--- getlink.js
+++ getlink.js
@@ -51,7 +51,7 @@ async function getCookieCsrf() {
     }, 1000 * 60 * 60);
   }
   loop = true;
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
   await page.goto('https://www.fshare.vn/site/login');
   await page.type('#loginform-email', config.email);
